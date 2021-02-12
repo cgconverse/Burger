cbe33004dc4e20647aac77426c68c5caa9b73ced
CGC <70499508+cgconverse@users.noreply.github.com>
2021-02-12 11:14:31 -0800

    created function to convert object key/value pairs to SQL syntax

--- config/orm.js
+++ config/orm.js
@@ -11,3 +11,22 @@ function printQuestionMarks(num) {
 //turns array into a string
   return arr.toString();
 }
+
+// Converting object key/value pairs to SQL syntax
+function objToSql(ob) {
+    var arr = [];
+  
+    // looping through the keys and pushing the key/value as a string into an array
+    for (var key in ob) {
+      var value = ob[key];
+      if (Object.hasOwnProperty.call(ob, key)) {
+        if (typeof value === "string" && value.indexOf(" ") >= 0) {
+          value = "'" + value + "'";
+        }
+        arr.push(key + "=" + value);
+      }
+    }
+  
+    // Converting the array of strings into a single string separated by commas
+    return arr.toString();
+  }
