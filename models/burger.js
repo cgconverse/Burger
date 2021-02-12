b213eef5dd24aaffb5e61ff7ad4eda94a1126ddd
CGC <70499508+cgconverse@users.noreply.github.com>
2021-02-12 11:39:12 -0800

    created variables for burger.js

---
+++ models/burger.js
@@ -0,0 +1,9 @@
+// Importing the ORM to create functions that will interact with the database
+var orm = require("../config/orm.js");
+
+var burger = {
+    all: function(cb) {
+        orm.all("burgers", function(res) {
+            cb(res);
+        });
+    },
