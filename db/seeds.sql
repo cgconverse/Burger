7841061255f4ffc13264460e4b1641d9c6001b63
CGC <70499508+cgconverse@users.noreply.github.com>
2021-02-12 11:35:10 -0800

    added seeds

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (name, eaten) VALUES ('MdDelicious Burger', false);
+INSERT INTO burgers (name, eaten ) VALUES ('AllStar Burger', false);
+INSERT INTO burgers (name, eaten) VALUES ('PoppySeed Burger', true);
+INSERT INTO burgers (name, eaten) VALUES ('ultimate Burger', false);
+INSERT INTO burgers (name, eaten) VALUES ('LosAngeles Burger', true);
