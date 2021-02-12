eef5974270df3870500da93ad107315e95fc03b6
CGC <70499508+cgconverse@users.noreply.github.com>
2021-02-12 11:58:37 -0800

    created post request

--- public/assets/js/burgers.js
+++ public/assets/js/burgers.js
@@ -11,15 +11,38 @@ $(function() {
         console.log(newEatenState)
         console.log(id)
 
-        // Send the PUT request.
+        // Sending the put request
         $.ajax("/api/burgers/" + id, {
             type: "PUT",
             data: newEatenState
         }).then(
             function() {
                 console.log("changed eaten to", newEaten);
+                // when the page is reloaded it will get the updated list
+                location.reload();
+            }
+        );
+    });
+
+    $(".create-form").on("submit", function(event) {
+        event.preventDefault();
+
+        var newBurger = {
+            name: $("#burger").val().trim(),
+            eaten: $("[name=eaten]:checked").val().trim()
+        };
+        console.log(newBurger)
+            
+        // Sending the post request
+        $.ajax("/api/burgers", {
+            type: "POST",
+            data: newBurger
+        }).then(
+            function() {
+                console.log("created new burger");
                 // Reload the page to get the updated list
                 location.reload();
             }
         );
     });
+});
