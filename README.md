dcfdb3634d6123799034102c171fb4c289fb1bae
CGC <70499508+cgconverse@users.noreply.github.com>
2021-02-12 12:22:27 -0800

    added description to README file

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # Burger
+
+## Description
+
+ Burger is a restaurant app that lets users input the names of burgers they'd like to eat.
--- server.js
+++ server.js
@@ -1,6 +1,26 @@
 var express = require("express");
 const dotenv = require("dotenv").config();
-
 var PORT = process.env.PORT || 8080;
-
 var app = express();
+
+// Serve static content from the public directory 
+app.use(express.static("public"));
+
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// setting handlebars
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Importing routes and providing access to the server
+var routes = require("./controllers/burgers_controller.js");
+
+app.use(routes);
+
+app.listen(PORT, function() {
+    console.log("App now listening at localhost:" + PORT);
+});
